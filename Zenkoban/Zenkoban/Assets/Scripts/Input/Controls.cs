cc80ebbe608fc4c3488d532e262a1719ba6f1610
Andrew Thompson <mnemonix.lstudios@gmail.com>
2020-07-06 15:25:37 +0100

    Added gamepad support.

--- Zenkoban/Zenkoban/Assets/Scripts/Input/Controls.cs
+++ Zenkoban/Zenkoban/Assets/Scripts/Input/Controls.cs
@@ -6,14 +6,12 @@ using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
 
-namespace Zenkoban.Input
+public class @Controls : IInputActionCollection, IDisposable
 {
-    public class @Controls : IInputActionCollection, IDisposable
+    public InputActionAsset asset { get; }
+    public @Controls()
     {
-        public InputActionAsset asset { get; }
-        public @Controls()
-        {
-            asset = InputActionAsset.FromJson(@"{
+        asset = InputActionAsset.FromJson(@"{
     ""name"": ""Controls"",
     ""maps"": [
         {
@@ -75,8 +73,30 @@ namespace Zenkoban.Input
                 },
                 {
                     ""name"": """",
+                    ""id"": ""03c9eaf1-ec07-4742-9b06-b28a5dded87b"",
+                    ""path"": ""<Gamepad>/dpad/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MoveUp"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
                     ""id"": ""cb775299-2e98-4d73-9222-21b4fa56e155"",
                     ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MoveDown"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""268e095f-e5a0-46ee-b113-91e160ec3499"",
+                    ""path"": ""<Gamepad>/dpad/down"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
@@ -97,8 +117,30 @@ namespace Zenkoban.Input
                 },
                 {
                     ""name"": """",
+                    ""id"": ""4460292f-c530-4412-aab4-2dddecc9a055"",
+                    ""path"": ""<Gamepad>/dpad/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MoveLeft"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
                     ""id"": ""1f64cdb0-21e1-427a-9421-4ebadeff5587"",
                     ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MoveRight"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""d7c8cc37-7399-45d6-8038-208d5d9580b6"",
+                    ""path"": ""<Gamepad>/dpad/right"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
@@ -116,136 +158,146 @@ namespace Zenkoban.Input
                     ""action"": ""Undo"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""e7beef8b-1985-4cee-9b2f-f6078066d98d"",
+                    ""path"": ""<Gamepad>/leftTrigger"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Undo"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
     ],
     ""controlSchemes"": []
 }");
-            // InGame
-            m_InGame = asset.FindActionMap("InGame", throwIfNotFound: true);
-            m_InGame_MoveUp = m_InGame.FindAction("MoveUp", throwIfNotFound: true);
-            m_InGame_MoveDown = m_InGame.FindAction("MoveDown", throwIfNotFound: true);
-            m_InGame_MoveLeft = m_InGame.FindAction("MoveLeft", throwIfNotFound: true);
-            m_InGame_MoveRight = m_InGame.FindAction("MoveRight", throwIfNotFound: true);
-            m_InGame_Undo = m_InGame.FindAction("Undo", throwIfNotFound: true);
-        }
-
-        public void Dispose()
-        {
-            UnityEngine.Object.Destroy(asset);
-        }
-
-        public InputBinding? bindingMask
-        {
-            get => asset.bindingMask;
-            set => asset.bindingMask = value;
-        }
-
-        public ReadOnlyArray<InputDevice>? devices
-        {
-            get => asset.devices;
-            set => asset.devices = value;
-        }
-
-        public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
-
-        public bool Contains(InputAction action)
-        {
-            return asset.Contains(action);
-        }
-
-        public IEnumerator<InputAction> GetEnumerator()
-        {
-            return asset.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        public void Enable()
-        {
-            asset.Enable();
-        }
-
-        public void Disable()
-        {
-            asset.Disable();
-        }
-
         // InGame
-        private readonly InputActionMap m_InGame;
-        private IInGameActions m_InGameActionsCallbackInterface;
-        private readonly InputAction m_InGame_MoveUp;
-        private readonly InputAction m_InGame_MoveDown;
-        private readonly InputAction m_InGame_MoveLeft;
-        private readonly InputAction m_InGame_MoveRight;
-        private readonly InputAction m_InGame_Undo;
-        public struct InGameActions
+        m_InGame = asset.FindActionMap("InGame", throwIfNotFound: true);
+        m_InGame_MoveUp = m_InGame.FindAction("MoveUp", throwIfNotFound: true);
+        m_InGame_MoveDown = m_InGame.FindAction("MoveDown", throwIfNotFound: true);
+        m_InGame_MoveLeft = m_InGame.FindAction("MoveLeft", throwIfNotFound: true);
+        m_InGame_MoveRight = m_InGame.FindAction("MoveRight", throwIfNotFound: true);
+        m_InGame_Undo = m_InGame.FindAction("Undo", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // InGame
+    private readonly InputActionMap m_InGame;
+    private IInGameActions m_InGameActionsCallbackInterface;
+    private readonly InputAction m_InGame_MoveUp;
+    private readonly InputAction m_InGame_MoveDown;
+    private readonly InputAction m_InGame_MoveLeft;
+    private readonly InputAction m_InGame_MoveRight;
+    private readonly InputAction m_InGame_Undo;
+    public struct InGameActions
+    {
+        private @Controls m_Wrapper;
+        public InGameActions(@Controls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @MoveUp => m_Wrapper.m_InGame_MoveUp;
+        public InputAction @MoveDown => m_Wrapper.m_InGame_MoveDown;
+        public InputAction @MoveLeft => m_Wrapper.m_InGame_MoveLeft;
+        public InputAction @MoveRight => m_Wrapper.m_InGame_MoveRight;
+        public InputAction @Undo => m_Wrapper.m_InGame_Undo;
+        public InputActionMap Get() { return m_Wrapper.m_InGame; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(InGameActions set) { return set.Get(); }
+        public void SetCallbacks(IInGameActions instance)
         {
-            private @Controls m_Wrapper;
-            public InGameActions(@Controls wrapper) { m_Wrapper = wrapper; }
-            public InputAction @MoveUp => m_Wrapper.m_InGame_MoveUp;
-            public InputAction @MoveDown => m_Wrapper.m_InGame_MoveDown;
-            public InputAction @MoveLeft => m_Wrapper.m_InGame_MoveLeft;
-            public InputAction @MoveRight => m_Wrapper.m_InGame_MoveRight;
-            public InputAction @Undo => m_Wrapper.m_InGame_Undo;
-            public InputActionMap Get() { return m_Wrapper.m_InGame; }
-            public void Enable() { Get().Enable(); }
-            public void Disable() { Get().Disable(); }
-            public bool enabled => Get().enabled;
-            public static implicit operator InputActionMap(InGameActions set) { return set.Get(); }
-            public void SetCallbacks(IInGameActions instance)
+            if (m_Wrapper.m_InGameActionsCallbackInterface != null)
             {
-                if (m_Wrapper.m_InGameActionsCallbackInterface != null)
-                {
-                    @MoveUp.started -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveUp;
-                    @MoveUp.performed -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveUp;
-                    @MoveUp.canceled -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveUp;
-                    @MoveDown.started -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveDown;
-                    @MoveDown.performed -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveDown;
-                    @MoveDown.canceled -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveDown;
-                    @MoveLeft.started -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveLeft;
-                    @MoveLeft.performed -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveLeft;
-                    @MoveLeft.canceled -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveLeft;
-                    @MoveRight.started -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveRight;
-                    @MoveRight.performed -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveRight;
-                    @MoveRight.canceled -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveRight;
-                    @Undo.started -= m_Wrapper.m_InGameActionsCallbackInterface.OnUndo;
-                    @Undo.performed -= m_Wrapper.m_InGameActionsCallbackInterface.OnUndo;
-                    @Undo.canceled -= m_Wrapper.m_InGameActionsCallbackInterface.OnUndo;
-                }
-                m_Wrapper.m_InGameActionsCallbackInterface = instance;
-                if (instance != null)
-                {
-                    @MoveUp.started += instance.OnMoveUp;
-                    @MoveUp.performed += instance.OnMoveUp;
-                    @MoveUp.canceled += instance.OnMoveUp;
-                    @MoveDown.started += instance.OnMoveDown;
-                    @MoveDown.performed += instance.OnMoveDown;
-                    @MoveDown.canceled += instance.OnMoveDown;
-                    @MoveLeft.started += instance.OnMoveLeft;
-                    @MoveLeft.performed += instance.OnMoveLeft;
-                    @MoveLeft.canceled += instance.OnMoveLeft;
-                    @MoveRight.started += instance.OnMoveRight;
-                    @MoveRight.performed += instance.OnMoveRight;
-                    @MoveRight.canceled += instance.OnMoveRight;
-                    @Undo.started += instance.OnUndo;
-                    @Undo.performed += instance.OnUndo;
-                    @Undo.canceled += instance.OnUndo;
-                }
+                @MoveUp.started -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveUp;
+                @MoveUp.performed -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveUp;
+                @MoveUp.canceled -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveUp;
+                @MoveDown.started -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveDown;
+                @MoveDown.performed -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveDown;
+                @MoveDown.canceled -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveDown;
+                @MoveLeft.started -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveLeft;
+                @MoveLeft.performed -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveLeft;
+                @MoveLeft.canceled -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveLeft;
+                @MoveRight.started -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveRight;
+                @MoveRight.performed -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveRight;
+                @MoveRight.canceled -= m_Wrapper.m_InGameActionsCallbackInterface.OnMoveRight;
+                @Undo.started -= m_Wrapper.m_InGameActionsCallbackInterface.OnUndo;
+                @Undo.performed -= m_Wrapper.m_InGameActionsCallbackInterface.OnUndo;
+                @Undo.canceled -= m_Wrapper.m_InGameActionsCallbackInterface.OnUndo;
+            }
+            m_Wrapper.m_InGameActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @MoveUp.started += instance.OnMoveUp;
+                @MoveUp.performed += instance.OnMoveUp;
+                @MoveUp.canceled += instance.OnMoveUp;
+                @MoveDown.started += instance.OnMoveDown;
+                @MoveDown.performed += instance.OnMoveDown;
+                @MoveDown.canceled += instance.OnMoveDown;
+                @MoveLeft.started += instance.OnMoveLeft;
+                @MoveLeft.performed += instance.OnMoveLeft;
+                @MoveLeft.canceled += instance.OnMoveLeft;
+                @MoveRight.started += instance.OnMoveRight;
+                @MoveRight.performed += instance.OnMoveRight;
+                @MoveRight.canceled += instance.OnMoveRight;
+                @Undo.started += instance.OnUndo;
+                @Undo.performed += instance.OnUndo;
+                @Undo.canceled += instance.OnUndo;
             }
         }
-        public InGameActions @InGame => new InGameActions(this);
-        public interface IInGameActions
-        {
-            void OnMoveUp(InputAction.CallbackContext context);
-            void OnMoveDown(InputAction.CallbackContext context);
-            void OnMoveLeft(InputAction.CallbackContext context);
-            void OnMoveRight(InputAction.CallbackContext context);
-            void OnUndo(InputAction.CallbackContext context);
-        }
+    }
+    public InGameActions @InGame => new InGameActions(this);
+    public interface IInGameActions
+    {
+        void OnMoveUp(InputAction.CallbackContext context);
+        void OnMoveDown(InputAction.CallbackContext context);
+        void OnMoveLeft(InputAction.CallbackContext context);
+        void OnMoveRight(InputAction.CallbackContext context);
+        void OnUndo(InputAction.CallbackContext context);
     }
 }
