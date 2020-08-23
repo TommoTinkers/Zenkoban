657ca8a5249d06a77cc7de0e2ee121da9f932398
Andrew Thompson <mnemonix.lstudios@gmail.com>
2020-08-23 21:57:14 +0100

    Carousel improvements.

--- Zenkoban/Zenkoban/Assets/Scripts/Runtime/UI/Carousel/CarouselMenu.cs
+++ Zenkoban/Zenkoban/Assets/Scripts/Runtime/UI/Carousel/CarouselMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using DG.Tweening;
@@ -42,6 +43,34 @@ namespace Zenkoban.Runtime.UI.Carousel
 
 			selectedIndex = 0;
 			panels[selectedIndex].localScale = Vector3.one;
+			
+			ChangeNonFocusedPanels(selectedIndex, float.Epsilon);
+		}
+
+		
+		[Button("CycleRight")]
+		public void CycleRight() => CycleTo(selectedIndex + 1, GameSettings.CarouselDuration);
+
+		[Button("CycleLeft")]
+		public void CycleLeft() => CycleTo(selectedIndex - 1, GameSettings.CarouselDuration);
+
+		[Button("Right 5")]
+		public void CycleRightFive()
+		{
+			var targetIndex = panels.ThisIndexOrMax(selectedIndex + 5);
+			CycleTo(targetIndex, GameSettings.CarouselDuration * 2.5f);
+		}
+
+		[Button("Left 5")]
+		public void CycleLeftFive()
+		{
+			var targetIndex = panels.ThisIndexOrMin(selectedIndex - 5);
+			CycleTo(targetIndex, GameSettings.CarouselDuration * 2.5f);
+		}
+
+		private static void Scale(Transform panel, float scaleMultiplier, float duration)
+		{
+			panel.DOScale(Vector3.one * scaleMultiplier, duration).Play();
 		}
 
 		private void CycleTo(int index, float totalDuration)
@@ -50,15 +79,13 @@ namespace Zenkoban.Runtime.UI.Carousel
 			{
 				return;
 			}
-
-			var panelToShrink = panels[selectedIndex];
+			
 			var panelToEnlarge = panels[index];
 
 			var numberOfPanelsToScroll = selectedIndex - index;
 
 			var newContainerPosition = panelContainer.position + Vector3.right * (numberOfPanelsToScroll * panelSpacing);
 			var anim = panelContainer.DOLocalMove(newContainerPosition, totalDuration);
-			
 			anim.onComplete += () =>
 			{
 				selectedIndex = index;
@@ -67,43 +94,69 @@ namespace Zenkoban.Runtime.UI.Carousel
 
 			isTransitioning = true;
 			
-			ScaleDown(panelToShrink, totalDuration);
-			ScaleUp(panelToEnlarge, totalDuration);
-
+			Scale(panelToEnlarge, 1, totalDuration);
+			
+			panelToEnlarge.GetComponent<CanvasGroup>().DOFade(1f, totalDuration).Play();
+			
+			ChangeNonFocusedPanels(index, totalDuration);
+			
 			anim.Play();
 		}
 
-		[Button("CycleRight")]
-		public void CycleRight() => CycleTo(selectedIndex + 1, GameSettings.CarouselDuration);
-		
-		[Button("CycleLeft")]
-		public void CycleLeft() => CycleTo(selectedIndex - 1, GameSettings.CarouselDuration);
-
-		[Button("Right 5")]
-		public void CycleRightFive()
-		{
-			var targetIndex = panels.ThisIndexOrMax(selectedIndex + 5);
-			CycleTo(targetIndex, GameSettings.CarouselDuration);
-		}
-		
-		[Button("Left 5")]
-		public void CycleLeftFive()
-		{
-			var targetIndex = panels.ThisIndexOrMin(selectedIndex - 5);
-			CycleTo(targetIndex, GameSettings.CarouselDuration);
-		}
-		
-		private void ScaleDown(Transform panel, float duration) => Scale(panel, nonFocusScale, duration);
-		private static void ScaleUp(Transform panel, float duration) => Scale(panel, 1f, duration);
-
-		private static void Scale(Transform panel, float scaleMultiplier, float duration)
-		{
-			panel.DOScale(Vector3.one * scaleMultiplier, duration).Play();
-		}
-		
 		private void HandleTransitionComplete()
 		{
 			isTransitioning = false;
+		}
+
+		private float GetAlphaBasedOnDistance(int distance) => alphasBasedOnDistance.ContainsKey(distance) ? alphasBasedOnDistance[distance] : 0f;
+
+		private float GetScaleBasedOnDistance(int distance)
+		{
+			CreateScalesDistanceTable();
+			return scalesBasedOnDistance.ContainsKey(distance) ? scalesBasedOnDistance[distance] : 0f;
+		}
+
+		private void CreateScalesDistanceTable()
+		{
+			scalesBasedOnDistance = scalesBasedOnDistance ?? new Dictionary<int, float>
+			{
+				{0, 1f},
+				{1, nonFocusScale},
+				{2, nonFocusScale * nonFocusScale},
+			};
+		}
+
+		private Dictionary<int, float> scalesBasedOnDistance;
+		private readonly Dictionary<int, float> alphasBasedOnDistance = new Dictionary<int, float>
+		{
+			{0, 1f},
+			{1, 0.8f},
+			{2, 0.5f},
+		};
+		
+		
+		private void ChangeNonFocusedPanels(int index, float totalDuration)
+		{
+			for (var x = 0; x < panels.Count; x++)
+			{
+				var dist = Math.Abs(index - x);
+				
+				FadePanel(x, dist, totalDuration);
+				ScalePanel(x, dist, totalDuration);
+			}
+		}
+
+		private void ScalePanel(int index, int distance, float totalDuration)
+		{
+			var scaleFactor = GetScaleBasedOnDistance(distance);	
+			Scale(panels[index], scaleFactor, totalDuration);
+		}
+
+		private void FadePanel(int index, int distance, float totalDuration)
+		{
+			var opacityScaleFactor = GetAlphaBasedOnDistance(distance);
+				
+			panels[index].GetComponent<CanvasGroup>().DOFade(opacityScaleFactor, totalDuration).Play();
 		}
 	}
 }
