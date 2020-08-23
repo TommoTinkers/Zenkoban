e967235eadda6da0c2196ee17709cafe77862837
Andrew Thompson <andrewthompson@logicstudios.net>
2020-08-23 15:37:19 +0100

    Refactored the cycling code in the carousel menu

---
+++ Zenkoban/Zenkoban/Assets/Scripts/Extensions/Utility/Collections/IListExtensions.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+
+namespace Zenkoban.Extensions.Utility.Collections
+{
+	public static class IListExtensions
+	{
+		public static bool IsNotLast<T>(this IList<T> list, int index)
+		{
+			return index < list.Count - 1;
+		}
+
+		public static bool IsNotFirst<T>(this IList<T> list, int index)
+		{
+			return index > 0 && list.Count >= 1;
+		}
+
+		public static bool IsValidIndex<T>(this IList<T> list, int index)
+		{
+			return list != null &&
+			       index >= 0 &&
+			       index < list.Count;
+		}
+
+		public static int ThisIndexOrMax<T>(this IList<T> list, int index)
+		{
+			if (list.IsValidIndex(index))
+			{
+				return index;
+			}
+
+			return list.Count - 1;
+		}
+
+		public static int ThisIndexOrMin<T>(this IList<T> list, int index)
+		{
+			return list.IsValidIndex(index) ? index : 0;
+		}
+	}
+}
--- Zenkoban/Zenkoban/Assets/Scripts/Runtime/UI/Carousel/CarouselMenu.cs
+++ Zenkoban/Zenkoban/Assets/Scripts/Runtime/UI/Carousel/CarouselMenu.cs
@@ -3,10 +3,12 @@ using System.Linq;
 using DG.Tweening;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using Zenkoban.Extensions.Utility.Collections;
 using Zenkoban.Settings;
 
 namespace Zenkoban.Runtime.UI.Carousel
 {
+	
 	public class CarouselMenu : MonoBehaviour
 	{
 		[SerializeField]
@@ -43,57 +45,62 @@ namespace Zenkoban.Runtime.UI.Carousel
 			panels[selectedIndex].localScale = Vector3.one;
 		}
 
-		[Button("CycleLeft")]
-		public void CycleLeft()
+		private void CycleTo(int index, float totalDuration)
 		{
-			if (selectedIndex < panels.Count - 1 && !isTransitioning)
+			if (!panels.IsValidIndex(index) || isTransitioning)
 			{
-				var anim = panelContainer.DOLocalMove(panelContainer.transform.position + (Vector3.left * panelSpacing), GameSettings.CarouselDuration);
-				anim.onComplete += (() => 
-						{
-							selectedIndex++;
-							HandleTransitionComplete();
-						});
-
-				
-				isTransitioning = true;
-
-				panels[selectedIndex].DOScale(Vector3.one * nonFocusScale, GameSettings.CarouselDuration).Play();
-
-				if ((selectedIndex + 1) <= panels.Count - 1)
-				{
-					panels[selectedIndex + 1].DOScale(Vector3.one, GameSettings.CarouselDuration).Play();
-				}
-				
-				anim.Play();
-				
-
+				return;
 			}
+
+			var panelToShrink = panels[selectedIndex];
+			var panelToEnlarge = panels[index];
+
+			var numberOfPanelsToScroll = selectedIndex - index;
+
+			var newContainerPosition = panelContainer.position + Vector3.right * (numberOfPanelsToScroll * panelSpacing);
+			var anim = panelContainer.DOLocalMove(newContainerPosition, totalDuration);
+			
+			anim.onComplete += () =>
+			{
+				selectedIndex = index;
+				HandleTransitionComplete();
+			};
+
+			isTransitioning = true;
+			
+			ScaleDown(panelToShrink, totalDuration);
+			ScaleUp(panelToEnlarge, totalDuration);
+
+			anim.Play();
+		}
+
+		[Button("CycleRight")]
+		public void CycleRight() => CycleTo(selectedIndex + 1, GameSettings.CarouselDuration);
+
+
+		[Button("CycleLeft")]
+		public void CycleLeft() => CycleTo(selectedIndex - 1, GameSettings.CarouselDuration);
+
+		[Button("Right 5")]
+		public void CycleRightFive()
+		{
+			var targetIndex = panels.ThisIndexOrMax(selectedIndex + 5);
+			CycleTo(targetIndex, GameSettings.CarouselDuration);
 		}
 		
-		[Button("CycleRight")]
-		public void CycleRight()
+		[Button("Left 5")]
+		public void CycleLeftFive()
 		{
-			if (selectedIndex > 0 && !isTransitioning)
-			{
-				var anim = panelContainer.DOLocalMove(panelContainer.transform.position + (Vector3.right * panelSpacing), GameSettings.CarouselDuration);
-				anim.onComplete += (() => 
-				{
-					selectedIndex--;
-					HandleTransitionComplete();
-				});
+			var targetIndex = panels.ThisIndexOrMin(selectedIndex - 5);
+			CycleTo(targetIndex, GameSettings.CarouselDuration);
+		}
+		
+		private void ScaleDown(Transform panel, float duration) => Scale(panel, nonFocusScale, duration);
+		private static void ScaleUp(Transform panel, float duration) => Scale(panel, 1f, duration);
 
-				panels[selectedIndex].DOScale(Vector3.one * nonFocusScale, GameSettings.CarouselDuration).Play();
-
-				if ((selectedIndex - 1) >= 0)
-				{
-					panels[selectedIndex - 1].DOScale(Vector3.one, GameSettings.CarouselDuration).Play();
-				}
-
-				
-				isTransitioning = true;
-				anim.Play();
-			}
+		private static void Scale(Transform panel, float scaleMultiplier, float duration)
+		{
+			panel.DOScale(Vector3.one * scaleMultiplier, duration).Play();
 		}
 		
 		private void HandleTransitionComplete()
